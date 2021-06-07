1d882ba775b8d45356a61390a3d7fe7a73fcc22b
Rodrigo Santa Eulalia <rsantaeulalia@medallia.com>
2021-06-07 20:30:55 -0300

    added create date function

--- server.js
+++ server.js
@@ -33,9 +33,8 @@ var listener = app.listen(process.env.PORT, function () {
 
 app.get("/api/:date?", function returnFile(req, res) {
   if(req.params.date){
-    console.log(req.params.date);
-    const date = new Date(isNaN(req.params.date) ? req.params.date : NumberLong(req.params.date));
-    if (isValidDate(date)) {
+    const date = createDate(req.params.date)
+    if (date) {
         res.json({ "unix": date.toString(), "utc": date.toGMTString() });
     } else {
         res.json({ "error": "Invalid Date" })
@@ -45,6 +44,25 @@ app.get("/api/:date?", function returnFile(req, res) {
   }
 });
 
-function isValidDate(d) {
-  return true;
+function createDate(date_string) {
+  const regexp = /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/;
+  const regexp2 = /^(\d{0,13})?$/;
+  let date;
+  switch (typeof date_string) {
+    case "string":
+      if (regexp2.test(date_string)) {
+        date = new Date(Number(date_string * 1e3)); 
+        break; 
+      }
+      if (regexp.test(date_string)) {
+        date = new Date(date_string);
+      } else {
+        return console.log("Invalid Date String");
+      }
+      break;
+    case "undefined":
+    default:
+      return undefined;
+  }
+  return date;
 }
